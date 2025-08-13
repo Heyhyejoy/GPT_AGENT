abd8134efabf3b71a4a7a1671920b9c04dc1830c
heyhyejoy <jhyeju1@gmail.com>
2025-08-13 08:52:33 +0300

    chapter 3

---
+++ Chapter/03/no_prompting.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    temperature=0.9,
+    messages=[
+        {"role": "system", "content": "너는 유치원생이야. 유치원생처럼 답변해 줘."},
+        {"role": "user", "content": "오리"},
+    ]
+)
+#python3 -u "no_prompting.py"
+
+print(response)
+
+print('----')
+print(response.choices[0].message.content) #response의 내용만 출력
